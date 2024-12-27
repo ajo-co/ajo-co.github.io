75df0e97f17dffdc5dad50cc5c9a58726d4e1f41
reza.shoja <reza.shoja@utechia.com>
2024-12-27 12:41:58 +0330

    fix build bug

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -3,7 +3,7 @@
 import React from "react";
 import { Resend } from "resend";
 import { validateString, getErrorMessage } from "@/lib/utils";
-import ContactFormEmail from "@/email/contact-form-email";
+// import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -24,22 +24,22 @@ export const sendEmail = async (formData: FormData) => {
   }
 
   let data;
-  try {
-    data = await resend.emails.send({
-      from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
-      subject: "Message from contact form",
-      reply_to: senderEmail,
-      react: React.createElement(ContactFormEmail, {
-        message: message,
-        senderEmail: senderEmail,
-      }),
-    });
-  } catch (error: unknown) {
-    return {
-      error: getErrorMessage(error),
-    };
-  }
+  // try {
+  //   data = await resend.emails.send({
+  //     from: "Contact Form <onboarding@resend.dev>",
+  //     to: "bytegrad@gmail.com",
+  //     subject: "Message from contact form",
+  //     reply_to: senderEmail,
+  //     react: React.createElement(ContactFormEmail, {
+  //       message: message,
+  //       senderEmail: senderEmail,
+  //     }),
+  //   });
+  // } catch (error: unknown) {
+  //   return {
+  //     error: getErrorMessage(error),
+  //   };
+  // }
 
   return {
     data,
