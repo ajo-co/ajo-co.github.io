eabdcfaaef954747a03049731d40fa2ab2874d3c
reza.shoja <reza.shoja@utechia.com>
2025-01-04 15:17:04 +0330

    fix header bug

--- components/header.tsx
+++ components/header.tsx
@@ -19,7 +19,7 @@ export default function Header({ lng }: { lng: string }) {
     <header className="z-[999] relative">
       <motion.div
         className={clsx(
-          "fixed top-0 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75",
+          "fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75",
           dir(lng) === "ltr" ? "sm:w-[775px]" : "sm:w-[44rem]"
         )}
         initial={{ y: -100, x: "-50%", opacity: 0 }}
